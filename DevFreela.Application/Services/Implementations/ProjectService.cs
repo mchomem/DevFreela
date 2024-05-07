d1e907bdf080c0e62857b6e92ccbcc29e724c901
Misael C. Homem <misael.homem@gmail.com>
2024-05-07 18:00:12 -0300

    feat: using Dapper package

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,19 +1,24 @@
-﻿using DevFreela.Application.InputModels;
+﻿using Dapper;
+using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Application.Services.Implementations
 {
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
 
-        public ProjectService(DevFreelaDbContext dbContext)
+        public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
 
         public int Create(NewProjectInputModel inputModel)
@@ -102,7 +107,18 @@ namespace DevFreela.Application.Services.Implementations
 
             project.Start();
 
-            _dbContext.SaveChanges();
+            //_dbContext.SaveChanges();
+
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "update Projects set Status = @status, StartedAt = @startedAt where Id = @id";
+
+                sqlConnection.Execute(script, new { status = project.Status, startedAt = project.StartedAt, id = id });
+
+
+            }
         }
 
         public void Update(UpdateProjectInputModel inputModel)
--- DevFreela.Application/Services/Implementations/SkillService.cs
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -1,25 +1,39 @@
-﻿using DevFreela.Application.Services.Interfaces;
+﻿using Dapper;
+using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Application.Services.Implementations;
 
 public class SkillService : ISkillService
 {
     private readonly DevFreelaDbContext _dbContext;
+    private readonly string _connectionString;
 
-    public SkillService(DevFreelaDbContext dbContext)
+    public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration )
     {
         _dbContext = dbContext;
+        _connectionString = configuration.GetConnectionString("DevFreelaCs");
     }
 
     public List<SkillViewModel> GetAll()
     {
-        var skills = _dbContext.Skills;
+        using (var sqlConnection = new SqlConnection(_connectionString))
+        {
+            sqlConnection.Open();
 
-        var skillsViewModel = skills
-            .Select(x => new SkillViewModel(x.Id, x.Description))
-            .ToList();
+            var script = "select Id, Description from Skills";
 
-        return skillsViewModel;
+            return sqlConnection.Query<SkillViewModel>(script).ToList();
+        }
+
+        //var skills = _dbContext.Skills;
+
+        //var skillsViewModel = skills
+        //    .Select(x => new SkillViewModel(x.Id, x.Description))
+        //    .ToList();
+
+        //return skillsViewModel;
     }
 }
